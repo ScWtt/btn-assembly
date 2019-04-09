e5d53895d63aa972dc1cce38564337b3b0939b6c
DyanWtt <461929783@qq.com>
2019-04-09 16:49:56 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <button-count
+      ref="countBtn"
+      @changeshow="changeshow"
+      :isShow="isShow"
+      :dis="dis"
+      bgcolor = '#345eee'
+    >{{submitContent}}</button-count>
+  </div>
+</template>
+
+<script>
+import buttonCount from "../src/components/button_count";
+export default {
+  name: "App",
+  data() {
+    return {
+      submitContent: "立即提交",
+      isShow: false,
+      dis: false
+    };
+  },
+  methods: {
+    changeshow() {
+      this.isShow = !this.isShow;
+      this.dis = !this.dis;
+      if(this.dis){
+        this.submitContent = "加载中";
+      }
+      let _this = this;
+      var setStyle = setTimeout(function() {
+        _this.isShow = false;
+        _this.dis = false;
+        _this.submitContent = "已经提交";
+      }, 3000);
+    }
+  },
+  components: {
+    buttonCount
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/button_count.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <button
+      class="submit_btn"
+      v-on:click="submitClick"
+      :disabled="dis"
+      :style="{backgroundColor:bgcolor,cursor:curstyle}"
+    >
+      <img class="loading-img" :src="btnImg" alt v-if="isShow">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    isShow: {
+      type: Boolean,
+      default: false
+    },
+    dis: {
+      type: Boolean,
+      default: false
+    },
+    bgcolor: {
+      type: "",
+      default: "green"
+    }
+  },
+  data() {
+    return {
+      btnImg: "../../static/images/loading.png"
+    };
+  },
+  methods: {
+    submitClick() {
+      this.$emit("changeshow");
+    }
+  },
+   computed:{
+    curstyle:function(){
+      return this.dis?"not-allowed":"pointer"
+    }
+  }
+};
+</script>
+<style scope>
+.submit_btn {
+  line-height: 30px;
+  height: 30px;
+  outline: none;
+  border: 1px solid #000;
+  border-radius: 5px;
+}
+.loading-img {
+  padding-right: 5px;
+  height: 20px;
+  width: 20px;
+}
+</style>
